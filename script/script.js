1b3635357377ae850520f1ef1dbd94885c7c41dd
VulgoLz <110253705+VulgoLz@users.noreply.github.com>
2023-07-16 22:47:30 -0300

    Primeira versão
    Pagina inicial com um carrossel

---
+++ script/script.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 120,
+    loop:false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    margin: 1,
+    nav:true,
+    centerMode: true,
+
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            
+            items:5
+        }
+    }
+});
